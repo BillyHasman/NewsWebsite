792241648cc282a496e465e05c265c27b6ebda8f
Billy Hasman <billyhasman08@gmail.com>
2023-11-01 22:26:16 +0700

    Update_add colors and remove themes: 'dark'

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,24 @@ export default {
     darkMode: "class",
 
     daisyui: {
-        themes: ["light", "dark"],
+        themes: ["light"],
     },
 
     theme: {
         extend: {
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+                white: "#ffffff",
+                purple: "#3f3cbb",
+                midnight: "#121063",
+                metal: "#565584",
+                tahiti: "#3ab7bf",
+                silver: "#ecebff",
+                "bubble-gum": "#ff77e9",
+                bermuda: "#78dcca",
+            },
+
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
